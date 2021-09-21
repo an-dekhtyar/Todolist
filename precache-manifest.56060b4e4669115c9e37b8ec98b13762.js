07fc5af7e4f67c0b39104fc795cb31370ccdfe9e
antdkhtr <antd3149@gmail.com>
2021-09-21 21:27:31 +0300

    Updates

---
+++ precache-manifest.56060b4e4669115c9e37b8ec98b13762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201e5532d4aa25b1837e1ca48ec0879f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8e26689fa248933db033",
+    "url": "/todolist/static/css/main.f6e654bb.chunk.css"
+  },
+  {
+    "revision": "968471e6513e8917d2b5",
+    "url": "/todolist/static/js/2.4160eb77.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.4160eb77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e26689fa248933db033",
+    "url": "/todolist/static/js/main.344e3d2e.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
